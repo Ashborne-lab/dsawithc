2a54bb99882827b5fadf9dcddf88a2e482f5d6f1
Ashborne-lab <138369125+Ashborne-lab@users.noreply.github.com>
2024-12-02 23:49:14 +0530

    iterative
    dsa

--- bst/traversal.c
+++ bst/traversal.c
@@ -1,5 +1,31 @@
 #include <stdio.h> 
 #include <stdlib.h> 
+  #define MAX 100
+  int top=-1;
+  void* stack[MAX];
+  void* push(void* val){
+     if(top==MAX-1){
+          printf("Stack is full\n");
+        exit(0) ;
+     }
+     stack[++top]=val;
+  }
+  void* pop(){
+     if(top==-1){
+          printf("Empty stack\n");
+          return NULL;
+     }
+     
+     return stack[top--];
+  }
+  void* peek(){
+     if(top==-1){
+          printf("Empty stack\n");
+          return NULL;
+     }
+     
+     return stack[top];
+  }
   
 
 typedef struct tnode 
@@ -14,6 +40,7 @@ typedef struct tnode
 
 tnode* createNode(int val) 
 { 
+
      tnode* new_Node = (tnode*) malloc(sizeof(tnode)); 
      if ( new_Node == NULL )
      {
@@ -25,7 +52,59 @@ tnode* createNode(int val)
      new_Node->right = NULL; 
   
      return(new_Node); 
-} 
+}
+
+
+
+void iterativepreorder(tnode* ptr){
+if(ptr==NULL)return;
+push(ptr);
+while(top!=-1){
+    tnode* current=(tnode*)pop();
+     printf("%d ", current->info);
+    if(current->right!=NULL)push(current->right);
+    if(current->left!=NULL)push(current->left);
+}
+
+
+}
+void iterativeinorder(tnode* ptr){
+     if(ptr==NULL)return ;
+tnode* node=ptr;
+while(1){
+     if(node!=NULL){
+          push(node);
+          node=node->left;
+     }
+     else{
+          if(top==-1)break;
+          node=(tnode*)pop();
+          printf("%d ",node->info);
+          node=node->right;
+     }
+}
+}
+void iterativepostorder(tnode* ptr){
+  if(ptr==NULL)return;
+  tnode* curr=ptr;
+tnode* lastVisited=NULL;
+
+  while(curr!=NULL || top!=-1){
+     if(curr!=NULL){
+          push(curr);
+          curr=curr->left;
+     }
+    else {
+            tnode* peekNode = (tnode*)peek();
+            if (peekNode->right != NULL && lastVisited != peekNode->right) {
+                curr = peekNode->right;
+            } else {
+                printf("%d ", peekNode->info);
+                lastVisited = (tnode*)pop();
+            }
+        }
+    }
+}
   
 void printPreorder(tnode* ptr);
 void printPostorder(tnode* ptr);
@@ -45,14 +124,17 @@ int main()
   
      printf("\nPreorder traversal of binary tree is \n"); 
      printPreorder(root); 
+       printf("\n Iterative Preorder traversal of binary tree is \n"); 
+     iterativepreorder(root);
   
      printf("\nInorder traversal of binary tree is \n"); 
      printInorder(root);   
-  
+  printf("\n Iterative Inorder traversal of binary tree is \n"); 
+      iterativeinorder(root); 
      printf("\nPostorder traversal of binary tree is \n"); 
      printPostorder(root); 
-  
-  
+   printf("\n Iterative postorder traversal of binary tree is \n"); 
+  iterativepostorder(root);
      return 0; 
 } 
 
